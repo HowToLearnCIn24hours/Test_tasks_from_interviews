131c4fbf529a5de6a06136194dd70abfd09c5b06
Kat <128542006+HowToLearnCIn24hours@users.noreply.github.com>
2023-11-13 15:38:47 +0300

    Delete task 1.sql

--- task 1.sql
+++
@@ -1,7 +0,0 @@
-﻿/*There is a table City_Population with a population of cities: CITY (city name), Population (population).
-Write an SQL query that displays a city with a minimum population.*/
-SELECT city, population 
-FROM city_population
-WHERE population = (
-SELECT MIN(population)
-FROM city_population);
